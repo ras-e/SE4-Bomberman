2b539e8744605c0289ecfdf0295e5ac0d181377d
Janno <janpe20@student.sdu.dk>
2022-03-28 11:27:12 +0200

    1.0.1
    A fix that'll allow the application to launch with map initiated in the core module

--- OSGiCore/src/main/java/dk/sdu/mmmi/bomberman/Game.java
+++ OSGiCore/src/main/java/dk/sdu/mmmi/bomberman/Game.java
@@ -2,27 +2,28 @@ package dk.sdu.mmmi.bomberman;
 
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.assets.loaders.resolvers.ExternalFileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import dk.sdu.mmmi.bomberman.common.data.Entity;
+import dk.sdu.mmmi.bomberman.common.data.World;
 
 public class Game implements ApplicationListener {
 
-    /*
+    public World world;
     TiledMap tiledMap;
-    TiledMapTileLayer layoutlayer2, layoutlayer3;
-    OrthographicCamera camera;
+    OrthographicCamera cam;
     OrthogonalTiledMapRenderer renderer;
-    private Texture player;
-    private SpriteBatch batch;
-    float Speed = 50;
-    float playerx = 70;
-    float playery = 50;
-    */
+    Texture texture;
+    SpriteBatch batch;
+
 
    public Game() {
         init();
@@ -30,7 +31,7 @@ public class Game implements ApplicationListener {
 
     public void init() {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Asteroids";
+        cfg.title = "Bomberman";
         cfg.width = 800;
         cfg.height = 600;
         cfg.resizable = false;
@@ -40,50 +41,26 @@ public class Game implements ApplicationListener {
 
     @Override
     public void create() {
-       /*
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1470,1340 );
-        camera.update();
-        tiledMap = new TmxMapLoader().load("LargeMap.tmx");
-        renderer = new OrthogonalTiledMapRenderer(tiledMap);
-
-        player = new Texture("cat.png");
-        batch = new SpriteBatch();
-        layoutlayer2 = (TiledMapTileLayer)tiledMap.getLayers().get(1);
-        layoutlayer3 = (TiledMapTileLayer)tiledMap.getLayers().get(2);
-        */
+       cam = new OrthographicCamera();
+       cam.setToOrtho(false, 1470, 1340);
+       cam.update();
+       tiledMap = new TmxMapLoader().load("/home/janpe20/Desktop/SE4-Bomberman/OSGiCore/src/main/resources/assets/smallMap.tmx");
+       renderer = new OrthogonalTiledMapRenderer(tiledMap);
+       texture = new Texture(Gdx.files.internal("/home/janpe20/Desktop/SE4-Bomberman/OSGiCore/src/main/resources/assets/jens.png").file().getAbsolutePath());
+       batch = new SpriteBatch();
     }
 
     @Override
     public void render() {
-      /*  renderer.setView(camera);
-        renderer.render();
-        batch.begin();
+       renderer.setView(cam);
+       renderer.render();
+       renderer.getBatch().begin();
+       batch.begin();
+       batch.draw(texture,32,32,55,55);
+       cam.update();
+       renderer.getBatch().end();
+       batch.end();
 
-        camera.update();
-        if(Gdx.input.isKeyPressed(Input.Keys.W)){
-            // Test if key is working
-            // System.out.println("W");
-            playery+= Gdx.graphics.getDeltaTime()*Speed;
-
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)){
-            // Test if key is working
-            // System.out.println("S");
-            playery-= Gdx.graphics.getDeltaTime()*Speed;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-            // Test if key is working
-            // System.out.println("A");
-            playerx -= Gdx.graphics.getDeltaTime() * Speed;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-            // Test if key is working
-            // System.out.println("D");
-            playerx += Gdx.graphics.getDeltaTime() * Speed;
-        }
-        batch.draw(player, playerx , playery);
-        batch.end(); */
     }
 
     @Override
