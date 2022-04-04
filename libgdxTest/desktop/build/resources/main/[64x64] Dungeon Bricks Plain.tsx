0fa0abce5a3b23b4ca2f5e96df02a16a0a538dcb
Johan Brockstedt <jobro19@student.sdu.dk>
2022-04-04 11:37:50 +0200

    Revert "dunno"
    This reverts commit 03963591ec5174dc4ed50fb58f83080a843ae469.

--- libgdxTest/desktop/build/resources/main/[64x64] Dungeon Bricks Plain.tsx
+++ libgdxTest/desktop/build/resources/main/[64x64] Dungeon Bricks Plain.tsx
@@ -1,14 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<<<<<<< Updated upstream
 <tileset version="1.8" tiledversion="1.8.2" name="[64x64] Dungeon Bricks Plain" tilewidth="64" tileheight="64" tilecount="56" columns="8">
  <image source="[64x64] Dungeon Bricks Plain.png" width="512" height="448"/>
-=======
-<tileset version="1.8" tiledversion="1.8.4" name="[64x64] Dungeon Bricks Plain" tilewidth="64" tileheight="64" tilecount="56" columns="8">
- <image source="[64x64] Dungeon Bricks Plain.png" width="512" height="448"/>
- <tile id="0">
-  <properties>
-   <property name="blocked" value=""/>
-  </properties>
- </tile>
->>>>>>> Stashed changes
 </tileset>
