28f8ade9a5afba3a40165f858fda7a003eb2d18e
rasmj20 <rasmj20@student.sdu.dk>
2022-05-14 15:28:41 +0200

    module fix

--- OSGiCollision/src/main/java/dk/sdu/mmmi/bomberman/collisiondetector/CollisionProcessor.java
+++ OSGiCollision/src/main/java/dk/sdu/mmmi/bomberman/collisiondetector/CollisionProcessor.java
@@ -5,12 +5,8 @@ import dk.sdu.mmmi.bomberman.common.data.World;
 import dk.sdu.mmmi.bomberman.common.services.IPostEntityProcessingService;
 
 public class CollisionProcessor implements IPostEntityProcessingService {
-    
-
-
-
     @Override
     public void process(GameData gameData, World world) {
-
+        
     }
 }
