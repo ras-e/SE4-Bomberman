d7cac21953fc9e0b6b9516daa06d71e83b4f1c74
ras-e <68448027+ras-e@users.noreply.github.com>
2022-05-14 15:16:38 +0200

    Added modules

---
+++ OSGiCollision/src/main/java/dk/sdu/mmmi/bomberman/collisiondetector/CollisionProcessor.java
@@ -0,0 +1,16 @@
+package dk.sdu.mmmi.bomberman.collisiondetector;
+
+import dk.sdu.mmmi.bomberman.common.data.GameData;
+import dk.sdu.mmmi.bomberman.common.data.World;
+import dk.sdu.mmmi.bomberman.common.services.IPostEntityProcessingService;
+
+public class CollisionProcessor implements IPostEntityProcessingService {
+    
+
+
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+    }
+}
---
+++ OSGiCommonMap/src/main/java/dk/sdu/mmmi/bomberman/Tile/Tile.java
@@ -0,0 +1,6 @@
+package dk.sdu.mmmi.bomberman.Tile;
+
+import dk.sdu.mmmi.bomberman.common.data.Entity;
+
+public class Tile extends Entity {
+}
